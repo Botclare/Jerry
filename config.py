529f558c462c6300e08c92c0a16d8e41065495d5
Botclare <botclare@gmail.com>
2025-02-16 19:58:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,6 +63,9 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
+COOKIES = getenv("COOKIES", None)
+# https://batbin.me
+
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
