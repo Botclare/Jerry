3366b72e72eb02677f5d8b68be11ec72ecfd1eb6
Botclare <botclare@gmail.com>
2025-03-02 17:28:50 +0530

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -11,7 +11,6 @@ from AnonXMusic.misc import sudo
 from AnonXMusic.plugins import ALL_MODULES
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
-from cookies import fetch_cookies
 
 
 async def init():
